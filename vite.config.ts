fba05c9712043cdb8f105f7dab5ba073ffdf7ec8
khaled-sobhy <khaledloda696@gmail.com>
2024-01-27 14:32:42 +0200

    Remove all PWA code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
