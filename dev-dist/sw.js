7990a4863c5772a757b1b4d42ae5eea64f6340e2
khaled-sobhy <khaledloda696@gmail.com>
2024-10-19 15:45:16 +0300

    Add date

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ubpp4eiv1io"
+    "revision": "0.5q5m58egkp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
